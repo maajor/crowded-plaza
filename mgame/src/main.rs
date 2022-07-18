eba417c525f3dadbda0d85fcccef806d49be6070
maajor <hello_myd@126.com>
2022-07-18 22:39:32 +0800

    spatial query and faction set

--- mgame/src/main.rs
+++ mgame/src/main.rs
@@ -9,7 +9,9 @@ use bevy::{
     window::WindowPlugin,
     winit::WinitPlugin,
 };
+use bevy_spatial::{KDTreeAccess2D, KDTreePlugin2D, SpatialAccess};
 use rand::{thread_rng, Rng};
+use std::{collections::HashMap, f32::consts::PI};
 // https://crowdedcity.io/
 
 #[derive(Component)]
@@ -19,9 +21,16 @@ struct Actor {
 
 #[derive(Component)]
 struct Pawn;
+#[derive(Component)]
+struct PlayerController;
 
 #[derive(Component)]
-struct Leader;
+struct OpponentController {
+    direction: Vec3,
+}
+
+const STEP: f32 = 0.03;
+const OPPONENT_MOVE_SCALE: f32 = 1.0;
 
 fn main() {
     App::new()
@@ -34,6 +43,7 @@ fn main() {
         .add_plugin(RenderPlugin::default())
         .add_plugin(CorePipelinePlugin::default())
         .add_plugin(PbrPlugin::default())
+        .add_plugin(KDTreePlugin2D::<Actor> { ..default() })
         .insert_resource(AmbientLight {
             brightness: 0.03,
             ..default()
@@ -44,6 +54,13 @@ fn main() {
         .add_system(actor_follow)
         .add_system(update_camera)
         .run();
+}
+
+type ActorSpace = KDTreeAccess2D<Actor>; // type alias for later
+
+#[derive(Clone)]
+struct FactionMaterialHandles {
+    faction_materials: HashMap<i32, Handle<StandardMaterial>>,
 }
 
 fn get_color_by_faction(faction: i32) -> Color {
@@ -59,46 +76,109 @@ fn get_color_by_faction(faction: i32) -> Color {
     }
 }
 
-// system: pawn get input and move
+// system: PlayerController get input and move
 fn player_move(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Actor, &mut Transform), With<Pawn>>,
+    mut player_query: Query<&mut Transform, With<PlayerController>>,
 ) {
-    let mut player = query.single_mut();
+    let mut player = player_query.single_mut();
     if keyboard_input.pressed(KeyCode::W) {
-        player.1.translation.x += 1.0;
-    } else if keyboard_input.just_pressed(KeyCode::A) {
-        player.1.translation.z += 1.0;
-    } else if keyboard_input.just_pressed(KeyCode::S) {
-        player.1.translation.x -= 1.0;
-    } else if keyboard_input.just_pressed(KeyCode::D) {
-        player.1.translation.z -= 1.0;
+        player.translation.x += STEP;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        player.translation.y += STEP;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        player.translation.x -= STEP;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        player.translation.y -= STEP;
     }
 }
 
-// system: path finding for leader
-fn leader_path_finding(query: Query<&mut Actor, With<Leader>>) {
-    for actor in query.iter() {
-        // todo!();
+// system: path finding for Pawn
+fn leader_path_finding(mut opponent_query: Query<(&mut Transform, &mut OpponentController)>) {
+    let mut rng = thread_rng();
+    for mut actor in opponent_query.iter_mut() {
+        let change_direction_random = rng.gen_range(0.0..1.0);
+        if change_direction_random < 0.01 {
+            let mut random_direction =
+                Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
+            random_direction = random_direction.normalize() * OPPONENT_MOVE_SCALE;
+            let new_direction = actor.1.direction + random_direction;
+            actor.1.direction = new_direction.normalize();
+        };
+        actor.0.translation += actor.1.direction * STEP;
     }
 }
 
 // system: actor follow and avoid collision
-fn actor_follow(query: Query<&mut Actor, (Without<Pawn>, Without<Leader>)>) {
-    for actor in query.iter() {
-        // todo!();
+fn actor_follow(
+    spatial_query: Res<ActorSpace>,
+    mut actor_set: Query<(
+        Entity,
+        &Transform,
+        &mut Actor,
+        &mut Handle<StandardMaterial>,
+    )>,
+    faction_materials: Res<FactionMaterialHandles>,
+) {
+    // https://github.com/bevyengine/bevy/issues/2495
+
+    let neighbor_threshold = 0.5;
+    let mut entity_id_to_faction: HashMap<Entity, i32> = HashMap::new();
+    // we cannot borrow actor_set twice as below we have to borrow it when do spatial query
+    // so cache entity_id_to_faction in this loop
+    for (entity, tr, _, _) in actor_set.iter() {
+        let mut faction_to_count: HashMap<i32, i32> = HashMap::new();
+        let neighbors = spatial_query.within_distance(tr.translation, neighbor_threshold);
+        for (_, neighbor_entity) in neighbors.iter() {
+            let (_, _, neighbor_actor, _) = actor_set.get(*neighbor_entity).unwrap();
+            if neighbor_actor.faction == -1 {
+                continue; // we skip neighbor no faction actor
+            }
+            if faction_to_count.contains_key(&neighbor_actor.faction) {
+                faction_to_count
+                    .entry(neighbor_actor.faction)
+                    .and_modify(|e| *e += 1);
+            } else {
+                faction_to_count.insert(neighbor_actor.faction, 1);
+            }
+        }
+        match faction_to_count.drain().max_by(|x, y| x.1.cmp(&y.1)) {
+            Some((faction, _)) => {
+                entity_id_to_faction.insert(entity, faction);
+            }
+            None => {}
+        }
+    }
+
+    for (entity, _, mut actor, mut mat) in actor_set.iter_mut() {
+        match entity_id_to_faction.get(&entity) {
+            Some(fac) => {
+                if *fac >= 0 {
+                    actor.faction = *fac;
+                    *mat = faction_materials
+                        .faction_materials
+                        .get(&fac)
+                        .unwrap()
+                        .clone();
+                }
+            }
+            None => {}
+        }
     }
 }
 
 // https://github.com/bevyengine/bevy/issues/2198
 fn update_camera(
-    player: Query<&Transform, (With<Pawn>, Without<Camera3d>)>,
-    mut camera: Query<&mut Transform, (With<Camera3d>, Without<Pawn>)>,
+    player_query: Query<&Transform, (With<PlayerController>, Without<Camera3d>)>,
+    mut camera_query: Query<&mut Transform, (With<Camera3d>, Without<PlayerController>)>,
 ) {
-    let pl = player.single();
-    let mut cam = camera.single_mut();
-    cam.translation = pl.translation + vec3(5.0, 10.0, 0.0);
-    cam.look_at(pl.translation, vec3(1.0, 0.0, 0.0));
+    let pl = player_query.single();
+    let mut cam = camera_query.single_mut();
+    cam.translation = pl.translation + vec3(-5.0, 0.0, 10.0);
+    cam.look_at(pl.translation, vec3(0.0, 0.0, 1.0));
 }
 
 fn setup(
@@ -106,6 +186,27 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    let region: f32 = 10.0;
+    let actor_count: i32 = 100;
+    let opponent_count: i32 = 1;
+
+    let mut faction_to_materials: HashMap<i32, Handle<StandardMaterial>> = HashMap::new();
+    for fac in -1..5 {
+        faction_to_materials.insert(
+            fac,
+            materials.add(StandardMaterial {
+                base_color: get_color_by_faction(fac),
+                perceptual_roughness: 0.8,
+                ..default()
+            }),
+        );
+    }
+    let handles = FactionMaterialHandles {
+        faction_materials: faction_to_materials,
+    };
+
+    commands.insert_resource(handles.clone());
+
     // camera
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_xyz(0.7, 0.7, 1.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
@@ -114,58 +215,91 @@ fn setup(
 
     // actors
     let mut rng = thread_rng();
-    for _ in 0..100 {
-        let x = rng.gen_range(-5.0..5.0);
-        let z = rng.gen_range(-5.0..5.0);
+    for _ in 0..actor_count {
+        let x = rng.gen_range(-region..region);
+        let y = rng.gen_range(-region..region);
         commands
             .spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
+                mesh: meshes.add(Mesh::from(shape::Capsule {
+                    radius: 0.1,
+                    ..default()
+                })),
                 material: materials.add(StandardMaterial {
                     base_color: get_color_by_faction(-1),
+                    perceptual_roughness: 0.8,
                     ..default()
                 }),
-                transform: Transform::from_xyz(x, 0.0, z),
+                transform: Transform::from_xyz(x, y, 0.0)
+                    .with_rotation(Quat::from_rotation_x(PI * 0.5)),
                 ..default()
             })
             .insert(Actor { faction: -1 });
     }
 
-    // leaders
-    for fac in 1..5 {
-        let x = rng.gen_range(-5.0..5.0);
-        let z = rng.gen_range(-5.0..5.0);
+    // opponents
+    for fac in 1..(opponent_count + 1) {
+        let x = rng.gen_range(-region..region);
+        let y = rng.gen_range(-region..region);
+        let mut direction = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
+        direction = direction.normalize();
         commands
             .spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
-                material: materials.add(StandardMaterial {
-                    base_color: get_color_by_faction(fac),
+                mesh: meshes.add(Mesh::from(shape::Capsule {
+                    radius: 0.1,
                     ..default()
-                }),
-                transform: Transform::from_xyz(x, 0.0, z),
+                })),
+                material: handles.faction_materials.get(&fac).unwrap().clone(),
+                transform: Transform::from_xyz(x, y, 0.0)
+                    .with_rotation(Quat::from_rotation_x(PI * 0.5)),
                 ..default()
             })
             .insert(Actor { faction: fac })
-            .insert(Leader);
+            .insert(Pawn)
+            .insert(OpponentController {
+                direction: direction,
+            });
     }
 
     // player
     let x = rng.gen_range(-5.0..5.0);
-    let z = rng.gen_range(-5.0..5.0);
+    let y = rng.gen_range(-5.0..5.0);
     commands
         .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
-            material: materials.add(StandardMaterial {
-                base_color: get_color_by_faction(0), // player's faction
+            mesh: meshes.add(Mesh::from(shape::Capsule {
+                radius: 0.1,
                 ..default()
-            }),
-            transform: Transform::from_xyz(x, 0.0, z),
+            })),
+            material: handles.faction_materials.get(&0).unwrap().clone(),
+            transform: Transform::from_xyz(x, y, 0.0)
+                .with_rotation(Quat::from_rotation_x(PI * 0.5)),
             ..default()
         })
         .insert(Actor { faction: 0 })
-        .insert(Pawn);
+        .insert(Pawn)
+        .insert(PlayerController);
 
+    // ground
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 100.0 })),
+        material: materials.add(StandardMaterial {
+            base_color: Color::rgba(1.0, 1.0, 1.0, 1.0),
+            perceptual_roughness: 0.8,
+            ..default()
+        }),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0)
+            .with_rotation(Quat::from_rotation_x(PI * 0.5)),
+        ..default()
+    });
+
+    // light
     commands.spawn_bundle(PointLightBundle {
-        transform: Transform::from_xyz(3.0, 8.0, 5.0),
+        transform: Transform::from_xyz(0.0, 0.0, 8.0),
+        point_light: PointLight {
+            intensity: 1600.0,
+            color: Color::WHITE,
+            shadows_enabled: true,
+            ..default()
+        },
         ..default()
     });
 }
