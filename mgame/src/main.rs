c11445222c34421d4a3c99659d0e34856339226d
maajor <hello_myd@126.com>
2022-07-19 23:38:55 +0800

    follow and move

--- mgame/src/main.rs
+++ mgame/src/main.rs
@@ -10,27 +10,31 @@ use bevy::{
     winit::WinitPlugin,
 };
 use bevy_spatial::{KDTreeAccess2D, KDTreePlugin2D, SpatialAccess};
-use rand::{thread_rng, Rng};
+use rand::{prelude::ThreadRng, thread_rng, Rng};
 use std::{collections::HashMap, f32::consts::PI};
 // https://crowdedcity.io/
 
 #[derive(Component)]
 struct Actor {
     faction: i32,
+    velocity: Vec3,
 }
 
 #[derive(Component)]
-struct Pawn;
+struct Pawn {
+    alive: bool,
+}
 #[derive(Component)]
 struct PlayerController;
 
 #[derive(Component)]
-struct OpponentController {
-    direction: Vec3,
-}
+struct OpponentController;
 
 const STEP: f32 = 0.03;
 const OPPONENT_MOVE_SCALE: f32 = 1.0;
+const PAWN_SPEED: f32 = 0.02;
+const WANDER_SPEED: f32 = 0.005;
+const NEIGHBOR_THRESHOLD: f32 = 0.5;
 
 fn main() {
     App::new()
@@ -49,10 +53,13 @@ fn main() {
             ..default()
         })
         .add_startup_system(setup)
-        .add_system(leader_path_finding)
-        .add_system(player_move)
-        .add_system(actor_follow)
-        .add_system(update_camera)
+        .add_system(move_player_system)
+        .add_system(change_direction_actor_system)
+        .add_system(change_direction_pawn_system)
+        .add_system(move_actor_system)
+        .add_system(follow_actor_system)
+        .add_system(update_camera_lookat_system)
+        .add_system(follow_pawn_system)
         .run();
 }
 
@@ -77,43 +84,92 @@ fn get_color_by_faction(faction: i32) -> Color {
 }
 
 // system: PlayerController get input and move
-fn player_move(
+fn move_player_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<&mut Transform, With<PlayerController>>,
+    mut player_query: Query<&mut Actor, With<PlayerController>>,
 ) {
     let mut player = player_query.single_mut();
+    let mut direction = player.velocity.normalize();
     if keyboard_input.pressed(KeyCode::W) {
-        player.translation.x += STEP;
+        direction.x += STEP;
     }
     if keyboard_input.pressed(KeyCode::A) {
-        player.translation.y += STEP;
+        direction.y += STEP;
     }
     if keyboard_input.pressed(KeyCode::S) {
-        player.translation.x -= STEP;
+        direction.x -= STEP;
     }
     if keyboard_input.pressed(KeyCode::D) {
-        player.translation.y -= STEP;
+        direction.y -= STEP;
+    }
+    player.velocity = direction.normalize() * PAWN_SPEED;
+}
+
+fn random_change_direction(mut actor: &mut Actor, rng: &mut ThreadRng, speed: f32) {
+    let change_direction_random = rng.gen_range(0.0..1.0);
+    if change_direction_random < 0.01 {
+        let mut random_direction =
+            Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
+        random_direction = random_direction.normalize() * OPPONENT_MOVE_SCALE;
+        let new_direction = actor.velocity + random_direction;
+        actor.velocity = new_direction.normalize() * speed;
+    };
+}
+
+fn change_direction_actor_system(mut actor_query: Query<&mut Actor, Without<Pawn>>) {
+    let mut rng = thread_rng();
+    for mut actor in actor_query.iter_mut() {
+        if actor.faction != -1 {
+            continue;
+        }
+        random_change_direction(&mut actor, &mut rng, WANDER_SPEED);
     }
 }
 
-// system: path finding for Pawn
-fn leader_path_finding(mut opponent_query: Query<(&mut Transform, &mut OpponentController)>) {
+fn change_direction_pawn_system(
+    mut opponent_query: Query<(&mut Actor, &Pawn), With<OpponentController>>,
+) {
     let mut rng = thread_rng();
-    for mut actor in opponent_query.iter_mut() {
-        let change_direction_random = rng.gen_range(0.0..1.0);
-        if change_direction_random < 0.01 {
-            let mut random_direction =
-                Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
-            random_direction = random_direction.normalize() * OPPONENT_MOVE_SCALE;
-            let new_direction = actor.1.direction + random_direction;
-            actor.1.direction = new_direction.normalize();
-        };
-        actor.0.translation += actor.1.direction * STEP;
+    for (mut actor, pawn) in opponent_query.iter_mut() {
+        if !pawn.alive {
+            continue;
+        }
+        random_change_direction(&mut actor, &mut rng, PAWN_SPEED);
     }
 }
+
+fn move_actor_system(mut actor_query: Query<(&mut Transform, &Actor)>) {
+    for (mut tr, actor) in actor_query.iter_mut() {
+        tr.translation += actor.velocity;
+    }
+}
+
+fn follow_pawn_system(
+    mut actor_query: Query<&mut Actor, Without<Pawn>>,
+    pawn_query: Query<(&Actor, &Pawn)>,
+) {
+    let mut faction_to_velocity: HashMap<i32, Vec3> = HashMap::new();
+    for (pawn_actor, pawn) in pawn_query.iter() {
+        if pawn.alive {
+            faction_to_velocity.insert(pawn_actor.faction, pawn_actor.velocity);
+        }
+    }
+    for mut actor in actor_query.iter_mut() {
+        if actor.faction != -1 {
+            match faction_to_velocity.get(&actor.faction) {
+                Some(v) => actor.velocity = *v,
+                None => {}
+            }
+        }
+    }
+}
+
+// follow leader direction
+// seperation
+// move to leader position
 
 // system: actor follow and avoid collision
-fn actor_follow(
+fn follow_actor_system(
     spatial_query: Res<ActorSpace>,
     mut actor_set: Query<(
         Entity,
@@ -124,14 +180,12 @@ fn actor_follow(
     faction_materials: Res<FactionMaterialHandles>,
 ) {
     // https://github.com/bevyengine/bevy/issues/2495
-
-    let neighbor_threshold = 0.5;
     let mut entity_id_to_faction: HashMap<Entity, i32> = HashMap::new();
     // we cannot borrow actor_set twice as below we have to borrow it when do spatial query
     // so cache entity_id_to_faction in this loop
     for (entity, tr, _, _) in actor_set.iter() {
         let mut faction_to_count: HashMap<i32, i32> = HashMap::new();
-        let neighbors = spatial_query.within_distance(tr.translation, neighbor_threshold);
+        let neighbors = spatial_query.within_distance(tr.translation, NEIGHBOR_THRESHOLD);
         for (_, neighbor_entity) in neighbors.iter() {
             let (_, _, neighbor_actor, _) = actor_set.get(*neighbor_entity).unwrap();
             if neighbor_actor.faction == -1 {
@@ -145,7 +199,10 @@ fn actor_follow(
                 faction_to_count.insert(neighbor_actor.faction, 1);
             }
         }
-        match faction_to_count.drain().max_by(|x, y| x.1.cmp(&y.1)) {
+        match faction_to_count
+            .drain()
+            .max_by(|(_, count1), (_, count2)| count1.cmp(&count2))
+        {
             Some((faction, _)) => {
                 entity_id_to_faction.insert(entity, faction);
             }
@@ -171,7 +228,7 @@ fn actor_follow(
 }
 
 // https://github.com/bevyengine/bevy/issues/2198
-fn update_camera(
+fn update_camera_lookat_system(
     player_query: Query<&Transform, (With<PlayerController>, Without<Camera3d>)>,
     mut camera_query: Query<&mut Transform, (With<Camera3d>, Without<PlayerController>)>,
 ) {
@@ -218,6 +275,7 @@ fn setup(
     for _ in 0..actor_count {
         let x = rng.gen_range(-region..region);
         let y = rng.gen_range(-region..region);
+        let dir = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
         commands
             .spawn_bundle(PbrBundle {
                 mesh: meshes.add(Mesh::from(shape::Capsule {
@@ -233,15 +291,17 @@ fn setup(
                     .with_rotation(Quat::from_rotation_x(PI * 0.5)),
                 ..default()
             })
-            .insert(Actor { faction: -1 });
+            .insert(Actor {
+                faction: -1,
+                velocity: dir.normalize() * WANDER_SPEED,
+            });
     }
 
     // opponents
     for fac in 1..(opponent_count + 1) {
         let x = rng.gen_range(-region..region);
         let y = rng.gen_range(-region..region);
-        let mut direction = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
-        direction = direction.normalize();
+        let dir = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
         commands
             .spawn_bundle(PbrBundle {
                 mesh: meshes.add(Mesh::from(shape::Capsule {
@@ -253,16 +313,18 @@ fn setup(
                     .with_rotation(Quat::from_rotation_x(PI * 0.5)),
                 ..default()
             })
-            .insert(Actor { faction: fac })
-            .insert(Pawn)
-            .insert(OpponentController {
-                direction: direction,
-            });
+            .insert(Actor {
+                faction: fac,
+                velocity: dir.normalize() * PAWN_SPEED,
+            })
+            .insert(Pawn { alive: true })
+            .insert(OpponentController {});
     }
 
     // player
     let x = rng.gen_range(-5.0..5.0);
     let y = rng.gen_range(-5.0..5.0);
+    let dir = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
     commands
         .spawn_bundle(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Capsule {
@@ -274,8 +336,11 @@ fn setup(
                 .with_rotation(Quat::from_rotation_x(PI * 0.5)),
             ..default()
         })
-        .insert(Actor { faction: 0 })
-        .insert(Pawn)
+        .insert(Actor {
+            faction: 0,
+            velocity: dir.normalize() * PAWN_SPEED,
+        })
+        .insert(Pawn { alive: true })
         .insert(PlayerController);
 
     // ground
